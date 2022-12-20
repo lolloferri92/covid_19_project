a7d59d2190230668a233eeda9b0c3a849eacee07
lollo2 <lolloferri92@gmail.com>
2022-12-20 18:27:33 +0100

    aggiunta gitignore e ed entry point

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ from plot_graph import*
 import tkinter as tk
 from read_data_from_url_csv import *
 
-while True:
-    x=input()
-    if x=="read_data_from_url_csv":
-        data=read_data_from_url_csv()
-    if x=="introduce_regional_infection":
-        introduce_regional_infection(data)
-    elif x=="plot_graph":
-        region_test=input("di quale regione vuoi vedere i dati e stampare il grafico?    ")
-        plot_graph(region_test,len(data.index))
-    elif x=="exit":
-        break
-print("grazie per aver usato il mio primo progetto")
+
+if __name__ == "__main__":
+    print("programma per visualizzazione delle statistiche del covid 19")
+    while True:
+        x=input()
+        if x=="read_data_from_url_csv":
+            data=read_data_from_url_csv()
+        elif x=="introduce_regional_infection":
+            introduce_regional_infection(data)
+        elif x=="plot_graph":
+            region_test=input("di quale regione vuoi vedere i dati e stampare il grafico?    ")
+            plot_graph(region_test,len(data.index))
+        elif x=="exit":
+            break
+    print("grazie per aver usato il mio primo progetto")
 
 
 
